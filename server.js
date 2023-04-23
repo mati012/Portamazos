2a18417f3943516b30a54e2b279b27e4a447482c
nega29 <ni.gonzaleza@duocuc.cl>
2023-04-23 17:28:21 -0400

    npm i static

--- server.js
+++ server.js
@@ -5,18 +5,21 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.set("view engine", "ejs"); 
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res)=>{
-    res.render('index');
+    res.render('login');
 });
 
 app.get("/registro", (req, res)=>{
     res.render("registro");
 });
+
 app.get("/login", (req, res)=>{
     res.render("login");
 });
-
 
 app.listen(PORT, () => {
 
