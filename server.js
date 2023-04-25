631af4bf727cb75299fdc3607563e13b29f618f8
mati012 <mati.quiero.arevalo@gmail.com>
2023-04-25 00:53:31 -0400

    a

--- server.js
+++ server.js
@@ -130,3 +130,12 @@ app.post(
   });
 
 
+  function ul(index) {
+	console.log('click!' + index)
+	
+	var underlines = document.querySelectorAll(".underline");
+
+	for (var i = 0; i < underlines.length; i++) {
+		underlines[i].style.transform = 'translate3d(' + index * 100 + '%,0,0)';
+	}
+}
